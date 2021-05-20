36b20ddef5cfe86b2b0675a5dfc45c416edf6183
JUNYOUN SHIN <jyshine3@gmail.com>
2021-05-20 17:43:32 +0900

    shinejy 2021 05 20 add annotation

--- src/main/java/com/bluff/celebrity/admin/controller/TestController.java
+++ src/main/java/com/bluff/celebrity/admin/controller/TestController.java
@@ -1,4 +1,9 @@
 package com.bluff.celebrity.admin.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class TestController {
+
 }
